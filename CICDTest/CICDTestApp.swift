9b2f5b5e23a73579f5408d57ab9e9ed7f9bb8b2d
Thulasi Ram Boddu <tulsra@gmail.com>
2023-04-04 15:54:34 +0530

    First commit

---
+++ CICDTest/CICDTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CICDTestApp.swift
+//  CICDTest
+//
+//  Created by Thulasi Ram Boddu on 04/04/23.
+//
+
+import SwiftUI
+
+@main
+struct CICDTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CICDTestTests/CICDTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  CICDTestTests.swift
+//  CICDTestTests
+//
+//  Created by Thulasi Ram Boddu on 04/04/23.
+//
+
+import XCTest
+@testable import CICDTest
+
+final class CICDTestTests: XCTestCase {
+
+    func testExample() throws {
+      XCTAssertEqual(2*2, 3)
+    }
+
+}
